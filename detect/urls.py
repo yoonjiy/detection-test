df8a8cf5a2b816ef71918cfb77fac7fa862a9a32
YOONJI KIM <stritegdc@gmail.com>
2022-03-19 00:13:12 +0900

    [fix]: 필요없는 url 삭제

--- detect/urls.py
+++ detect/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('detect', views.detect, name="detect"),
-    path('result', views.show_result, name="result"),
 ]
